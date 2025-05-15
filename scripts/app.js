c1043c46d07b59429205cadfc61f68b104060e1a
Shashank Singh <shashank4414@gmail.com>
2025-05-15 10:43:33 -0400

    Corrections to files

--- scripts/app.js
+++ scripts/app.js
@@ -1,72 +1,82 @@
-//let countryURL = "json/country.json";
-//let countryURL = "json/countryNoNeighbours.json";
-let countryURL = "json/countryBonus.json";
-//let countryURL = "json/countryNoData.json";
+let url = "JSON/country.json";
+let api = "https://restcountries.com/v3.1/all"
 
-let countrydetailsURL = "https://restcountries.com/v3.1/all";
-let country = [];        
-
-// Load and sort country.json data
-$.getJSON(countryURL, function(data){
-    country = data.sort((a,b)=>{
-        if (a.country>b.country) return -1;
-        else if (a.country<b.country) return 1;
-        else return 0;
-    });  
-    }
-);
-
-// Load Web data using Async Fetch/Promice 
-async function fetchdata(sourceURL){
+// function to fetch json
+async function fetchdata(sourceURL) {
     let response = await fetch(sourceURL);
-    if (response.ok != true){
+    if (!response.ok) {
         throw new Error(response.statusText);
     }
-    return await response.json();
+    return await response.json(); //return response as json
 }
 
-// Determine if border key exists within element
-haveNeighbours = function(element,keyname){
-    for (let k of Object.keys(element)){
-        if (k==keyname){ return true; }
-    }
-    return false;
 
-};
-// Button Event Listener
-document.getElementById("btnDisplay").addEventListener("click", function(){
+// Event listener for the button click
+document.getElementById("btnDisplay").addEventListener("click", function () {
     let divOutput = document.getElementById("output");
+    divOutput.innerHTML = ""; // clear output area
     let outputString = "";
-    // Fetch web data when country JSON contains data
-    if (country.length>0){
-        fetchdata(countrydetailsURL)
+
+    let countries = []; // array for local json
+    let countriesAll = []; // array for api json
+
+    fetchdata(url) // fetch from local json
         .then(
-            data=>{
-                let countryFound=[];
-                country.forEach(c=>{ // Loop through country json data 
-                    outputString+=`<br>Country:${c.country}<br>&nbspCities: ${c.city==undefined?"None":c.city}<br>`;
-                    data.forEach(CD=>{ // Loop through Web Data
-                        if (CD.name.common == c.country){ // Matching Country json with web data
-                            countryFound.push(CD);
-                            if (haveNeighbours(CD,"borders")) // If country has neighbours then get neighbour country common name and display them
-                                { outputString+= `&nbsp&nbspNeighbours:${CD.borders.map(cb=>data.filter(ct=>ct.cca3==cb).pop().name.common)}<br>`; }
-                            else { outputString+="&nbsp&nbspNeighbours: None <br>";}
+            data => {
+                data.forEach(object => {
+                    countries.push(object); // adds each country object to array
+                });
+                // sort the objects in descending order by country name
+                countries.sort((a, b) => b.country.localeCompare(a.country));
+                return fetchdata(api); // fetch from api json
+            })
+        .then( // chaining .then methods ensures forEach loop doesn't run prematurely
+            data => {
+                data.forEach(element => {
+                    countriesAll.push(element);
+                });
+
+                if (countries.length == 0) { // if local json has no country data
+                    divOutput.innerHTML = `<p>No Country Data is Available to View</p>`;
+                }
+
+                countries.forEach(country => { // iterates over countries array
+                    let matchedCountry = countriesAll.find(c => c.name.common == country.country);
+                    if (matchedCountry) { // checks if there is a match
+                        let borderNames = (matchedCountry.borders || []).map(countryCode => { // incase object has no borders key
+                            let borderCountry = countriesAll.find(c => c.cca3 == countryCode);
+                            return borderCountry.name.common;
+                        });
+
+                        if (borderNames.length === 0) {
+                            borderNames.push("None"); // response for no borders
                         }
-                    });  
-                    if (countryFound.length == 0){ // Display message when a match is not found
-                        outputString+="&nbsp&nbspCountry Not Found in Web Data! <br>";
-                    }
-                    else{ countryFound = []; // Reset found array when match was found
-                    }
-                    divOutput.innerHTML = outputString;
-                })
-            }         
-            )
-        .catch(err=>divOutput.innerHTML = err)
-    }
-    else{ // Display message when no data is found in Country json
-        divOutput.innerHTML = "<br>&nbsp&nbspNo Country Data is Available to View"
-    }
 
+                        console.log(borderNames);
+                        // create the output string for matched countries
+                        outputString = `
+                            <div>
+                                <p>Country: ${country.country}</p>
+                                <p>&nbsp;Cities: ${country.cities.join(", ")}</p>
+                                <p>&nbsp;&nbsp;Neighbors: ${borderNames.join(", ")}</p>
+                            </div>
+                            <br>`;
+                        divOutput.innerHTML += outputString;
+                    } else {
+                        // create the output string if no match found
+                        outputString = `
+                            <div>
+                                <p>Country: ${country.country}</p>
+                                <p>&nbsp;Cities: ${country.cities.join(", ")}</p>
+                                <p>&nbsp;&nbsp;Country not Found in Web Data!</p>
+                            </div>
+                            <br>`;
+                        divOutput.innerHTML += outputString;
+                    }
+                });
+            })
+        .catch(err => divOutput.innerHTML = err);
 
+    console.log(countries);
+    console.log(countriesAll);
 });
